c70136e7415ac7235859e50f026ba4e4ed86346d
glenmccarthy <gb.mccarthy@gmail.com>
2015-10-07 00:12:16 +0100

    Adding api to get a particular message

--- src/main/java/org/glen/mccarthy/messenger/resources/MessageResource.java
+++ src/main/java/org/glen/mccarthy/messenger/resources/MessageResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -22,5 +23,11 @@ public class MessageResource {
 		return service.getAllMessages();
 	}
 	
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Message getMessage(@PathParam("messageId")long messageId){
+		return service.getMessage(messageId);
+	}
 	
 }
